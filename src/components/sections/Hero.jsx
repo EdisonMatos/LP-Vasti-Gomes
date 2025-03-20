ecd7ed9344f6ac77a2a2b2213fb299b5f7ef8bea
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-20 12:14:01 -0300

    fix: Altera a cor da Hero na Lpi

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -8,7 +8,7 @@ const whatsappContactLink = `${content.texts.links.ctaWhatsapp}`;
 export default function Hero({ appDownloadButtons }) {
   return (
     <div
-      className="w-full bg-center bg-repeat font-mainFont bg-gradient-to-b from-[#000000] to-secondary "
+      className="w-full bg-center bg-repeat font-mainFont bg-[#242730] "
       id="home"
     >
       <div className="relative z-10 flex w-full bg-black bg-opacity-0 items-left">
