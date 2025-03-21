03aa47421eeda36440f4c9cd542096c631071c93
Edison Matos <edison.matos@live.com>
2025-03-21 15:33:06 -0300

    feat: Atualiza feature corrigido

--- src/components/sections/FeatureWithIcons.jsx
+++ src/components/sections/FeatureWithIcons.jsx
@@ -12,7 +12,7 @@ import Icon4 from "../../assets/imgs/icons/icon4.png";
 
 export default function FeaturesWithIcons() {
   return (
-    <SectionArea id="service" className="squares" paddingtop={false}>
+    <SectionArea id="service" className="squares" paddingtop={true}>
       <SectionHeader
         className="text-center"
         miniTitle={content.texts.features.miniTag}
